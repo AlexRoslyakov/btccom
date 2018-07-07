351155bf5cd339e79dfd220d249bc01b2ffdd41f
Aleksandr Rosliakov <alex.roslyakov@gmail.com>
2018-07-07 13:06:26 +0200

    minimal UI

--- btccom/ViewController.swift
+++ btccom/ViewController.swift
@@ -8,11 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet weak var sellTableView: UITableView!
+    @IBOutlet weak var buyTableView: UITableView!
+    @IBOutlet weak var matchTableView: UITableView!
 
+    private let defaultTableViewCellIdentifier = "DefaultTableViewCellIdentifier"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        self.sellTableView.register(UITableViewCell.self, forCellReuseIdentifier: defaultTableViewCellIdentifier)
+        self.buyTableView.register(UITableViewCell.self, forCellReuseIdentifier: defaultTableViewCellIdentifier)
+        self.matchTableView.register(UITableViewCell.self, forCellReuseIdentifier: defaultTableViewCellIdentifier)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +30,44 @@ class ViewController: UIViewController {
     }
 
 
+    // MARK: UITableView
+
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if (tableView == self.sellTableView ||
+            tableView == self.buyTableView) {
+            return 20
+        }
+        else if (tableView == self.matchTableView) {
+            return 30
+        }
+        else {
+             precondition(false, "Unknown UITableView")
+        }
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: defaultTableViewCellIdentifier, for: indexPath)
+
+        if (tableView == self.sellTableView) {
+            cell.textLabel?.text = "Sell"
+        }
+        else
+            if (tableView == self.buyTableView) {
+                cell.textLabel?.text = "Buy"
+        }
+            else if (tableView == self.matchTableView) {
+                cell.textLabel?.text = "Match"
+
+        }
+            else {
+                precondition(false, "Unknown UITableView")
+        }
+
+        return cell
+    }
 }
 
