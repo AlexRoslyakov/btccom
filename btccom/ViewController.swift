227140ea4c908ee3ae4a17c190705a2479e2f972
Aleksandr Rosliakov <alex.roslyakov@gmail.com>
2018-07-07 12:14:33 +0200

    Initial Commit

---
+++ btccom/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  btccom
+//
+//  Created by aleksandr.rosliakov on 07/07/2018.
+//  Copyright © 2018 Alex Roslyakov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
